60c8cb1b9c738467656985e7df74704c9abddc5b
Jim Lawton <jim.lawton@s3group.com>
2015-11-06 09:05:22 +0000

    RDKCMF-2461  Update from Comcast source (master).
     * add Bluetooth Core and Manager Yocto Repo
    
    Change-Id: I7aa6a1dd376a3b4b0ef251ccc6317846f707e9f0
    Source: https://gerrit.teamccp.com/rdk/components/generic/bluetooth_mgr/generic
    SHA1: 713c471cab17d9dd62c427c397800858b9c3cee2

---
+++ src/main.c
@@ -0,0 +1,55 @@
+/**
+ * @file main.c
+ *
+ * @description This file defines bluetooth manager's main function
+ *
+ * Copyright (c) 2015  Comcast
+ */
+#include <stdlib.h>
+#include <signal.h>
+
+
+/*----------------------------------------------------------------------------*/
+/*                             Function Prototypes                            */
+/*----------------------------------------------------------------------------*/
+//static void __terminate_listener(int value);
+static void sig_handler(int sig);
+
+
+/*----------------------------------------------------------------------------*/
+/*                             External Functions                             */
+/*----------------------------------------------------------------------------*/
+int main(int argc,char *argv[])
+{
+	const char* debugConfigFile = NULL;
+	signal(SIGTERM, sig_handler);
+	signal(SIGINT, sig_handler);
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
+	signal(SIGSEGV, sig_handler);
+	signal(SIGBUS, sig_handler);
+	signal(SIGKILL, sig_handler);
+	signal(SIGFPE, sig_handler);
+	signal(SIGILL, sig_handler);
+	signal(SIGQUIT, sig_handler);
+	signal(SIGHUP, sig_handler);
+	signal(SIGALRM, sig_handler);
+
+	while(1)
+        {
+           sleep(1);
+        }
+	return 1;
+}
+
+/*----------------------------------------------------------------------------*/
+/*                             Internal functions                             */
+/*----------------------------------------------------------------------------*/
+static void sig_handler(int sig)
+{
+
+        signal(sig, SIG_DFL );
+        kill(getpid(), sig );
+
+}
+
